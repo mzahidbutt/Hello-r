020d2a4a402e8cfd65f1fc5f7a1a1c3ef2041408
Zahid Butt <muhammadzahidbutt@gmail.com>
2022-02-03 11:58:11 +0500

    02 Feb 2022 Few Lines Added

--- hello.r
+++ hello.r
@@ -1,3 +1,10 @@
 # This is first Hello program
 # Print Hello world
-print("Hello Gitgub Program)
+print("Hello Gitgub Program")
+
+#Assignment 
+name = "Zahid"
+print(name)
+print("First program display of R programming.")
+
+print("Ending statement")
